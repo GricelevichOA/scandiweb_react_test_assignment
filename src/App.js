69d18d87f5a6704c844af621f2f89c606bdfe93e
Oleg <gricelevich.oleg@gmail.com>
2022-02-12 14:56:30 +0300

    navbar rewrite

--- src/App.js
+++ src/App.js
@@ -5,51 +5,30 @@ import Navbar from "./Components/Navbar/Navbar";
 import Cart from "./Pages/Cart/Cart";
 import ProductDescription from "./Pages/ProductDescription/ProductDescription";
 import ProductList from "./Pages/ProductList/ProductList";
-import { client } from "./graphql/client.js";
-import { GET_CATEGORIES, GET_CURRENCIES } from "./graphql/queries.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      currentCurrency: {},
-      categories: [],
-      currencies: [],
+      currentCurrency: "",
+      currentCategory: "clothes",
     };
   }
 
-  componentDidMount() {
-    client
-      .query({
-        query: GET_CATEGORIES,
-      })
-      .then((res) => {
-        this.setState({
-          categories: [...res.data.categories],
-        });
-      });
-
-    client
-      .query({
-        query: GET_CURRENCIES,
-      })
-      .then((res) => {
-        this.setState({
-          currencies: [...res.data.currencies],
-        });
-      });
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div className="App">
-        <Navbar
-          categories={this.state.categories}
-          currencies={this.state.currencies}
-        />
+        <Navbar />
         <Routes>
-          <Route path="/" element={<ProductList />} />
+          <Route
+            path="/"
+            element={
+              <ProductList currentCategory={this.state.currentCategory} />
+            }
+          />
           <Route path="/product/:id" element={<ProductDescription />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,35 +1,54 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
 import logo from "../../Images/logo_transparent.svg";
 import cartLogo from "../../Images/empty_cart.svg";
+import { Query } from "@apollo/client/react/components";
+import { GET_CATEGORIES, GET_CURRENCIES } from "../../graphql/queries";
 
 export default class Navbar extends Component {
   render() {
     return (
       <div className="nav">
-        <div className="nav__links">
-          {this.props.categories.map((cat) => {
+        <Query query={GET_CATEGORIES}>
+          {({ loading, error, data }) => {
+            if (loading) return <h2>Loading...</h2>;
+            console.log(data);
             return (
-              <span className="nav__link" key={cat.name}>
-                {cat.name}
-              </span>
+              <div className="nav__links">
+                {data.categories.map((cat) => {
+                  return (
+                    <span className="nav__link" key={cat.name}>
+                      {cat.name}
+                    </span>
+                  );
+                })}
+              </div>
             );
-          })}
-        </div>
+          }}
+        </Query>
         <div className="nav__logo">
           <img src={logo} alt="logo" />
         </div>
         <div className="nav__actions">
-          <select className="nav__action">
-            {this.props.currencies.map((cur) => {
+          <Query query={GET_CURRENCIES}>
+            {({ loading, error, data }) => {
+              if (loading) return <h3>Loading...</h3>;
+              console.log(data);
               return (
-                <option key={cur.label}>
-                  {cur.symbol} {cur.label}
-                </option>
+                <select className="nav__action">
+                  {data.currencies.map((cur) => {
+                    return (
+                      <option key={cur.label}>
+                        {cur.symbol} {cur.label}
+                      </option>
+                    );
+                  })}
+                </select>
               );
-            })}
-          </select>
+            }}
+          </Query>
+
           <div className="nav__action">
             <img src={cartLogo} alt="Cart" />
           </div>
